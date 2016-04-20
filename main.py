989739d3a1698c71cd3a81b49f533651cbabbc13
Krzysztof Kraszewski <krzysztof@kraszewski.me>
2016-04-20 19:27:44 +0200

    Changed input to python3 and added progress bar

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
+import time
+import progressbar
+
 def Main():
-	print('=====Scrackle=====')
+	print('##########Scrackle##########')
 
 	f = open('slowa.txt', 'r')
 	words = f.readlines()
 	f.close()	
 	print('Database contains ' + str(len(words)) + ' words.')
-	letters = raw_input('Gimme letterz: ')
+	letters = input('Gimme letterz: ')
 	letters = list(letters)
+	available_words = []
 
-	for i, word in enumerate(words):
-		#print(str(i) + '/' + str(len(words)) + ' words checked.')
-		word = word.replace('\n', '').replace('\r', '')
-		count = 0
-		if len(word) <= len(letters):
-			l = len(word)
-			word_test = word
+	with progressbar.ProgressBar(max_value=len(words)) as bar:
+		for i, word in enumerate(words):
+			#print(str(i) + '/' + str(len(words)) + ' words checked.')
+			word = word.replace('\n', '').replace('\r', '')
+			count = 0
+			if len(word) <= len(letters):
+				l = len(word)
+				word_test = word
 
-			for j, letter in enumerate(letters):
-				if letter in word_test:
-					count += 1
-					word_test = word_test.replace(letter, '')
-			if count == l:
-				print(word + ' can be build from ' + str(letters))
+				for j, letter in enumerate(letters):
+					if letter in word_test:
+						count += 1
+						word_test = word_test.replace(letter, '')
+				if count == l:
+					available_words.append(word)
+			if i % 10000 == 0:
+				bar.update(i)
+
+	print('I found ' + str(len(available_words)) + ' words you can build: ')
+	for i, word in enumerate(available_words):
+		print(str(i + 1) + '. ' + word)
 
 Main()
